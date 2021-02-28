bfb5c8aaee0c378b51ecc231d97cc61232c3ad5b
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-02-28 21:52:56 +0700

    Update dtb.inc.php

--- dtb.inc.php
+++ dtb.inc.php
@@ -16,32 +16,10 @@ if(!$pg)
 			$query = 'select * from users';
 			$result = pg_query($pg, $query);
 			# Display data column by column
-			$i = 0;
-			echo '<html><body><table><tr>';
-			while ($i < pg_num_fields($result))
-			{
-				$fieldName = pg_field_name($result, $i);
-				echo '<td>' . $fieldName . '</td>';
-				$i = $i + 1;
-			}
-			echo '</tr>';
-			# Display data row by row
-			$i = 0;
-			while ($row = pg_fetch_row($result)) 
-			{
-				echo '<tr>';
-				$count = count($row);
-				$y = 0;
-				while ($y < $count)
-				{
-					$c_row = current($row);
-					echo '<td>' . $c_row . '</td>';
-					next($row);
-					$y = $y + 1;
-				}
-				echo '</tr>';
-				$i = $i + 1;
-			}
-			pg_free_result($result);
-
-			echo '</table></body></html>';
+			  $pg = "SELECT * FROM users WHERE username = '$username' AND password = '$password"
+        $users = pg_query($conn,$pg);
+        if(pg_run_row($user) > 0)
+        {echo"Login successfully!"}
+        else {
+            echo"Wrong Information!"
+        }
