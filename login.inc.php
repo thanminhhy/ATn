b451349b1958bdcaa4b96d841a4f252e9bc3dacd
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-02-28 21:55:42 +0700

    Update login.inc.php

--- login.inc.php
+++ login.inc.php
@@ -1,17 +1,11 @@
 <?php
-  require_once 'dtb.inc.php';
-        require_once 'functions.inc.php';
     if(isset($_POST["submit"]))
     {
         $username = $_POST["uid"];
         $pwd = $_POST["pwd"];
-        $pg = "SELECT * FROM users WHERE username = '$username' AND password = '$password"
-        $users = pg_query($conn,$pg);
-        if(pg_run_row($user) > 0)
-        {echo"Login successfully!"}
-        else {
-            echo"Wrong Information!"
-        }
+
+        require_once 'dtb.inc.php';
+        require_once 'functions.inc.php';
         
         if(emptyInputLogin($username, $pwd) !== false)
         {
