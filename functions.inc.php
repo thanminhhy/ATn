16c16d893df05c72b2578db20b1b5da659ea0a3c
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-02-28 22:16:51 +0700

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -1,1 +1,13 @@
 
+function emptyInputLogin($username,$pwd)
+{
+    $result;
+    if(empty($username) || empty($pwd))
+    {
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
