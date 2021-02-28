3c403a5d43ee51e9516309c9e90c5f3318e0d806
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-02-28 16:49:51 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>ATN Shop</title>
+	<link rel = "stylesheet" type="text/css" href="nav-bar for ATN.css">
+</head>
+<body>
+	<nav>
+		<div>
+			<ul>
+				<li><a class="ATN-brand" href="#">ATN Shop</a>
+          		<span class="luna-toggler-icon"></span>
+          		</button></li>
+				<li><a href = "Home.php">Home</a></li>
+				<li><a href = "Tutorials.php">Tutorials</a></li>
+				<li><a href = "About.php">About</a></li>
+				<li><a href = "signup.php">Sign Up</a></li>
+				<li><a href = "login.php">Login</a></li>
+			</ul>
+		</div>	
+	</nav>
+	<div class="body">
