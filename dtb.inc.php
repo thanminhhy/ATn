d0d936c6a232447740402b68044591cf5998b3d5
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-02-28 17:22:01 +0700

    Update dtb.inc.php

--- dtb.inc.php
+++ dtb.inc.php
@@ -12,3 +12,36 @@ if(!$pg)
 {
     die("Connection failed: " . pg_connect_error());
 }
+# Get data by query
+			$query = 'select * from test_lab6';
+			$result = pg_query($pg, $query);
+			# Display data column by column
+			$i = 0;
+			echo '<html><body><table><tr>';
+			while ($i < pg_num_fields($result))
+			{
+				$fieldName = pg_field_name($result, $i);
+				echo '<td>' . $fieldName . '</td>';
+				$i = $i + 1;
+			}
+			echo '</tr>';
+			# Display data row by row
+			$i = 0;
+			while ($row = pg_fetch_row($result)) 
+			{
+				echo '<tr>';
+				$count = count($row);
+				$y = 0;
+				while ($y < $count)
+				{
+					$c_row = current($row);
+					echo '<td>' . $c_row . '</td>';
+					next($row);
+					$y = $y + 1;
+				}
+				echo '</tr>';
+				$i = $i + 1;
+			}
+			pg_free_result($result);
+
+			echo '</table></body></html>';
