618f2e509c251321ce20b704b7320ee845f37b36
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-02-28 22:20:45 +0700

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -1,13 +1,4 @@
 <?php
-function uidExists($conn, $username){
-    $pg = "SELECT * FROM user_information WHERE user_name = ?;";
-    $stmt = pg_stmt_init($conn);
-    if(!pg_prepare($pg))
-    {
-        header("location: ../login.php?error=stmtfailed");
-        exit();
-    }
-}
 
 function emptyInputLogin($username,$pwd)
 {
