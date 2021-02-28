b5ef91548ddb9a85b3071e992d00e3cbeb4fa137
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-02-28 16:54:10 +0700

    Create functions.inc.php

---
+++ functions.inc.php
@@ -0,0 +1,35 @@
+<?php
+
+function uidExists($conn, $username){
+    $pg = "SELECT * FROM user_information WHERE user_name = ?;";
+    $stmt = pg_stmt_init($conn);
+    if(!pg_prepare($pg))
+    {
+        header("location: ../signup.php?error=stmtfailed");
+        exit();
+    }
+}
+
+function emptyInputLogin($username,$pwd)
+{
+    $result;
+    if(empty($username) || empty($pwd))
+    {
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+
+function loginUser($conn, $username, $pwd)
+{
+    $uidExists = uidExists($conn, $username);
+
+    if($uidExists == false)
+    {
+        header("location: ../login.php?error=wronglogin");
+        exit();
+    }
+}
