3c73bf513c4cf26bf78c7489355474777fe35197
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 09:14:45 +0700

    Update login.inc.php

--- login.inc.php
+++ login.inc.php
@@ -3,7 +3,19 @@
     {
         $username = $_POST["uid"];
         $pwd = $_POST["pwd"];
-
+        if($username == 'staff' $$ $pwd =='12345')
+        {
+        header("location: ../staff.php");
+        }
+        elseif ($username == 'boss' $$ $pwd =='090512345')
+        {	
+        header("location: ../boss.php");
+        }
+        else{
+        echo"incorrect username and password"
+            exit();
+        }
+        require_once 'dtb.inc.php';
         require_once 'functions.inc.php';
         
         if(emptyInputLogin($username, $pwd) !== false)
@@ -11,4 +23,10 @@
             header("location: ../login.php?error=emptyinput");
             exit();
         }
+        loginUser($conn, $username, $pwd);
+    }
+    else{
+        header("location: ../login.php");
+        exit();
+    }
 ?>
