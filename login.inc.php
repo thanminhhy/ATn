5dd4d616b1b4d3b73fdc5d04fd53f7141f0231da
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 10:28:03 +0700

    Update login.inc.php

--- login.inc.php
+++ login.inc.php
@@ -4,17 +4,20 @@
     {
         $uid = $_POST["uid"];
         $pwd = $_POST["pwd"];
-        if($uid == 'staff' $$ $pwd =='12345')
-        {
-        header("location: ../staff.php");
-        }
-        elseif ($uid == 'boss' $$ $pwd =='090512345')
-        {	
-        header("location: ../boss.php");
-        }
-        else{
-        echo"incorrect username and password"
-            exit();
+        $host = "ec2-54-235-139-166.compute-1.amazonaws.com";
+        $db = "d2aln19jkoqgc5";
+        $user = "dbzoashpxvqhmp";
+        $pw = "1d18b71cc013c47de2c56d8e5970d0aab6ed70c04d1b2d2f6ef73ae37b9cfdb8";
+
+        $conn_string ="host=$host port=5432 dbname=$db user=$user password=$pw";
+        $pg = pg_connect($conn_string);
+
+        $query = "'select * from users where username = '$uid' and password = 'pwd'";
+        $rs = pg_query($pg,$query)
+            if(pg_num_rows($rs) > 0)
+            {
+                header("location: ../boss.php")
+            }
     }
     else{
         header("location: ../login.php");
