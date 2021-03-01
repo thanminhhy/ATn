8f300cafb1f1dfe8a0d660c7450e37a6a9ba3fd5
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 09:13:14 +0700

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
         <h2 style="margin-left: 30px;">Log In</h2>
         <div class="container">
             <div class="login-form">
-                <form action="login.inc.php" method="post">
+                <form action="login.php" method="post">
                     <input type="text" name="uid" placeholder="UserName...">
                     <input type="password" name="pwd" placeholder="PassWord...">
                     <button type="submit" name="submit">Login</button>
