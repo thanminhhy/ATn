26ed8bc5fb30c1808e1d49fd4052d625947a6230
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 16:48:42 +0700

    Update AddProduct.php

--- AddProduct.php
+++ AddProduct.php
@@ -1,16 +1,17 @@
 <?php
-    $host = "ec2-54-235-139-166.compute-1.amazonaws.com";
-    $db = "d2aln19jkoqgc5";
-    $user = "dbzoashpxvqhmp";
-    $pw = "1d18b71cc013c47de2c56d8e5970d0aab6ed70c04d1b2d2f6ef73ae37b9cfdb8";
-
-    $conn_string ="host=$host port=5432 dbname=$db user=$user password=$pw";
-    $pg = pg_connect($conn_string);
-
-    if(!$pg)
-    {
-        die("Connection failed: " . pg_connect_error());
-    }
+	# Heroku credential 
+			$host_heroku = "ec2-18-206-84-251.compute-1.amazonaws.com";
+			$db_heroku = "d8k42dnhtd0o9i";
+			$user_heroku = "crmjpgdtqgprga";
+			$pw_heroku = "0d86d0fb5f24be75ffb6728bb2ffaa6762b75489e8923fda3cdf71c519a99d67";
+			# Create connection to Heroku Postgres
+			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			$pg_heroku = pg_connect($conn_string);
+			
+			if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
     } else {
       $product_id = $_POST['Id'];
       $product_name = $_POST['Productname'];
@@ -19,12 +20,12 @@
     }
    $query = "INSERT INTO Products (product_id, product_name, quantity, price) 
     VALUES('$product_id', '$product_name', '$quantity', '$price');";
-   $result = pg_query($pg, $query);
+   $result = pg_query($pg_heroku, $query);
    if ($result) {
       echo "<script>alert('Record added succesfully!');</script>";
       header('refresh: 3; url=staff.html');
    } else {
       echo ("ERROR + $query") . pg_errormessage($query);
     }
-    pg_close($pg);
+    pg_close($pg_heroku);
 ?>
