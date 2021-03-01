5cabff5f4590339d1764cf85b45be31b70d43642
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 09:08:28 +0700

    Update login.php

--- login.php
+++ login.php
@@ -14,6 +14,20 @@
         </div>
         </section>
         <?php
+          $username = $_POST["uid"];
+          $pwd = $_POST["pwd"];
+	    if($username == 'staff' $$ $pwd =='12345')
+	    {
+		    header("location: ../staff.php");
+	    }
+	    elseif ($username == 'boss' $$ $pwd =='090512345')
+	    {	
+		    header("location: ../boss.php");
+	    }
+	    else{
+		    echo"incorrect username and password"
+			    exit();
+	    }
             if(isset($_GET["error"]))
             {
                 if($_GET["error"] == "emptyinput"){
