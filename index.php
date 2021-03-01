23a895cc692813299d62c62f3632e248e975cda6
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 11:08:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
 session_start();
-$username = $_POST['Username'];
-$password = $_POST['Password'];
-    if ($username == 'staff' && $password == '123') {
-        header("location:staff.html");
-    } elseif ($username == 'boss' && $password == '12345') {
-        header("location:boss.html");
-    } else
-    {
-        require "login.html";
-    }
+header("location:login.html");
 ?>
