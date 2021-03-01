236184857df14d5409122e7925b6d2df330800a0
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 12:03:47 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,19 +2,13 @@
 session_start();
 $username = $_POST['Username'];
 $password = $_POST['Password'];
-require_once 'functions.inc.php';
     if ($username == 'admin' && $password == '123') {
         header("location:staff.html");
     } elseif ($username == 'boss' && $password == '123') {
         header("location:boss.html");
     } else
     {
-        echo "incorrect username and password";
+        echo "<p>incorrect username and password</p>";
          header("location:login.html");
     }
-    if(isset($_GET["error"]))
-            {
-                if($_GET["error"] == "emptyinput"){
-                    echo "<p>Fill in all fields!</p>";
-                }
 ?>
