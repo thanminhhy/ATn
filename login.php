1900208714418ff96a74b3ca85f8e81e94c6cdb3
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 09:13:54 +0700

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
         <h2 style="margin-left: 30px;">Log In</h2>
         <div class="container">
             <div class="login-form">
-                <form action="login.php" method="post">
+                <form action="login.inc.php" method="post">
                     <input type="text" name="uid" placeholder="UserName...">
                     <input type="password" name="pwd" placeholder="PassWord...">
                     <button type="submit" name="submit">Login</button>
@@ -14,20 +14,6 @@
         </div>
         </section>
         <?php
-          $username = $_POST["uid"];
-          $pwd = $_POST["pwd"];
-	    if($username == 'staff' $$ $pwd =='12345')
-	    {
-		    header("location: ../staff.php");
-	    }
-	    elseif ($username == 'boss' $$ $pwd =='090512345')
-	    {	
-		    header("location: ../boss.php");
-	    }
-	    else{
-		    echo"incorrect username and password"
-			require"login.php";
-	    }
             if(isset($_GET["error"]))
             {
                 if($_GET["error"] == "emptyinput"){
