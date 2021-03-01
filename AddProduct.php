762a3a59db5772ab50b545ab66655937bbb1f597
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 16:57:23 +0700

    Update AddProduct.php

--- AddProduct.php
+++ AddProduct.php
@@ -1,49 +1,28 @@
 <?php
-	echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
-			# Heroku credential 
-			$host_heroku = "ec2-18-206-84-251.compute-1.amazonaws.com";
-			$db_heroku = "d8k42dnhtd0o9i";
-			$user_heroku = "crmjpgdtqgprga";
-			$pw_heroku = "0d86d0fb5f24be75ffb6728bb2ffaa6762b75489e8923fda3cdf71c519a99d67";
-			# Create connection to Heroku Postgres
-			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-			$pg_heroku = pg_connect($conn_string);
-			
-			if (!$pg_heroku)
-			{
-				die('Error: Could not connect: ' . pg_last_error());
-			}
-			# Get data by query
-			$query = 'select * from products';
-			$result = pg_query($pg_heroku, $query);
-			# Display data column by column
-			$i = 0;
-			echo '<html><body><table><tr>';
-			while ($i < pg_num_fields($result))
-			{
-				$fieldName = pg_field_name($result, $i);
-				echo '<td>' . $fieldName . '</td>';
-				$i = $i + 1;
-			}
-			echo '</tr>';
-			# Display data row by row
-			$i = 0;
-			while ($row = pg_fetch_row($result)) 
-			{
-				echo '<tr>';
-				$count = count($row);
-				$y = 0;
-				while ($y < $count)
-				{
-					$c_row = current($row);
-					echo '<td>' . $c_row . '</td>';
-					next($row);
-					$y = $y + 1;
-				}
-				echo '</tr>';
-				$i = $i + 1;
-			}
-			pg_free_result($result);
+   $connect = pg_connect("ec2-54-235-139-166.compute-1.amazonaws.com
+  dbname=d2aln19jkoqgc5
+  port=5432
+  user=dbzoashpxvqhmp
+  password=1d18b71cc013c47de2c56d8e5970d0aab6ed70c04d1b2d2f6ef73ae37b9cfdb8
+   sslmode=require");
 
-			echo '</table></body></html>';
+   if ($connect === false) {
+      die("ERROR: Something went wrong with conenction!");
+    } else {
+      $product_id = $_POST['Id'];
+      $product_name = $_POST['Productname'];
+      $quanitty = $_POST['Quantity'];
+      $price = $_POST['Price'];
+
+    }
+    $query = "INSERT INTO Product (product_id, product_name, quantity, price) 
+    VALUES('$product_id', '$product_name', '$quantity', '$price');";
+    $result = pg_query($connect, $query);
+    if ($result) {
+      echo "<script>alert('Record added succesfully!, Refresh');</script>";
+      header('refresh: 3; url=staff.html');
+    } else {
+      echo ("ERROR + $query") . pg_errormessage($query);
+    }
+    pg_close($connect);
 ?>
