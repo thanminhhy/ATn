3ed0468ec748b8c30e16ce9e10e79ac71b0efc2f
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2021-03-01 09:16:05 +0700

    Update login.inc.php

--- login.inc.php
+++ login.inc.php
@@ -15,18 +15,4 @@
         echo"incorrect username and password"
             exit();
         }
-        require_once 'dtb.inc.php';
-        require_once 'functions.inc.php';
-        
-        if(emptyInputLogin($username, $pwd) !== false)
-        {
-            header("location: ../login.php?error=emptyinput");
-            exit();
-        }
-        loginUser($conn, $username, $pwd);
-    }
-    else{
-        header("location: ../login.php");
-        exit();
-    }
 ?>
